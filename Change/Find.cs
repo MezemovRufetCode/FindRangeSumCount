eba794d449fd457493a74476b43b8c1991e3d5b4
MezemovRufetCode <rufatam@code.edu.az>
2021-10-03 23:47:03 +0400

    Add project files.

---
+++ Change/Find.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Change
+{
+    class Find
+    {
+        public int FindSum(int n,int m)
+        {
+            int sum = 0;
+            for(int i = n; i < m; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+        public int FindCount(int n,int m)
+        {
+            int count = 0;
+            for(int i = n; i < m; i++)
+            {
+                count++;
+            }
+            return count;
+        }
+    }
+}
---
+++ Change/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Change
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Include range:");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Find f1 = new Find();
+            Console.WriteLine($"Sum of numbers:{f1.FindSum(a,b)}\nCount of range numbers:{f1.FindCount(a,b)}");
+ 
+        }
+    }
+}
